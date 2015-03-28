82d488347accd3f729218df610a2c27273ff3e80
Robin <robin-wagner@web.de>
2015-03-28 17:50:38 +0100

    Solutions for first set of exercises

---
+++ 01_TheBasics.rb
@@ -0,0 +1,52 @@
+# Exercise 1
+
+"Robin" + "Wagner"
+
+# Exercise 2
+
+number = 1234
+
+puts number / 1000
+puts number % 1000 / 100
+puts number % 100 / 10
+puts number % 10
+
+# Exercise 3
+
+movies = { jaws: 1975, 
+          anchorman: 2004,
+          man_of_steel: 2013,
+          a_beautiful_mind: 2001,
+          the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+# Exercise 4
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+dates[0]
+dates[1]
+dates[2]
+dates[3]
+dates[4]
+
+# Exercise 5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Exercise 6
+
+3.43 * 3.43
+23.43 * 23.43
+0.34345 * 0.34345
+
+# Exercise 7
+# Opening bracket without closing bracket.
