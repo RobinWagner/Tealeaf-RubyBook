2e3e1c9f96d006aeb78f0f8a8f76634b109b795e
Robin <robin-wagner@web.de>
2015-03-29 17:23:02 +0200

    Add solutions for exercises in chapter 2

---
+++ 02_Variables/name.rb
@@ -0,0 +1,19 @@
+# Exercise 1
+
+puts 'Enter your name, please:'
+
+name = gets.chomp
+puts "Welcome, #{name}!"
+
+# Exercise 3
+10.times { puts name }
+
+# Exercise 4
+
+puts 'Please enter your first name:'
+first_name = gets.chomp
+
+puts 'Please enter your last name:'
+last_name = gets.chomp
+
+puts 'Your full name is: ' +  first_name + ' ' + last_name
