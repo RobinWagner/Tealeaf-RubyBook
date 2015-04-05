6b489cae884ba69efc2f035c7986d35ea5132f7e
Robin <robin-wagner@web.de>
2015-04-05 10:20:56 +0200

    Finish exercises for chapter 9

---
+++ 09_MoreStuff/exercise1.rb
@@ -0,0 +1,9 @@
+def check_match(word)
+  puts word if word =~ /lab/
+end
+
+check_match("laboratory")
+check_match("experiment")
+check_match("Pans Labyrinth")
+check_match("elaborate")
+check_match("polar bear")
