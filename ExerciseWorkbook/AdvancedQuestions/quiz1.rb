f206c7997d9bc2e8787e8b37af71df5c8f18ba9a
Robin <robin-wagner@web.de>
2015-04-07 12:37:02 +0200

    Solution for quiz 1 (advanced questions)

---
+++ ExerciseWorkbook/AdvancedQuestions/quiz1.rb
@@ -0,0 +1,113 @@
+# Exercise 1
+
+if false
+  greeting = “hello world”
+end
+
+p greeting
+
+# Exercise 2
+
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting  #  => "hi there"
+puts greetings
+
+# Exercise 3
+
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# Exercise 4
+
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
+  end
+
+  uuid
+end
+
+p generate_UUID
+
+# Exercise 5
+
+statement = "Herman Munster is a BIG BIG man." 
+
+class String
+  def map_words!
+    punctuation = self.match(/\p{Punct}$/)[0] || ''
+    words = self.split(/\W/)
+    words.map! { |word| yield word }
+    self.replace(words.join(' ') + punctuation)
+  end
+end
+
+p statement.map_words! { |word| word.reverse }
+
+# Exercise 6
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size != 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false if !is_a_number?(word)
+  end
+  return true
+end
